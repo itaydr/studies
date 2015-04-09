62d2c65e8d75d9f503d5aa0741a0a8fec6ada3df
Itay Dressler <itaydr@mobli.com>
2015-04-09 17:12:47 +0300

    Added Quanta parameter for scheduler.

--- Ass 1/echo.c
+++ Ass 1/echo.c
@@ -5,13 +5,10 @@
 int
 main(int argc, char *argv[])
 {
-  //int i;
+  int i;
 
-//  for(i = 1; i < argc; i++)
-//    printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
-  printf(1, "before fg");
-  fg(2);
-  printf(1, "afer fg");
+  for(i = 1; i < argc; i++)
+    printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
   exit(EXIT_STATUS_OK);
     
    // return -3;
